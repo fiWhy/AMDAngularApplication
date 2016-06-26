85c92b1040b9a3398372cd32d50657494083b3dc
Buryachkovsky Denis <fiwhy@mail.ru>
2016-06-26 22:05:19 +0300

    Api is done

--- dev/core/constants/config/config.constant.ts
+++ dev/core/constants/config/config.constant.ts
@@ -5,11 +5,12 @@ export function ConfigConstant() {
         documentRoot: 'app',
         modulesRoot: 'app/modules',
         mainPaths: {
-            menu: apiUrl + 'menu/:name',
+            //menu: apiUrl + 'menu/:name',
             authorize: apiUrl + 'login',
             cars: apiUrl + 'cars/:id',
             posts: apiUrl + 'posts/:id',
-            shop: apiUrl + 'shop/:id?include=colors,sizes,images,groups'
+            shop: apiUrl + 'shop/:id?include=colors,sizes,images,groups',
+            menu: apiUrl + 'menu/:id?include=children'
             // authorize: 'google.com.ua'
         },
         language: 'en',
